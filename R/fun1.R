2cb185312ec5afebadf43ad08ce1df30a2afd47d
Victor Caldas <v.e.a.caldas@rug.nl>
2015-05-21 11:25:33 +0200

    Initial commit

---
+++ R/fun1.R
@@ -0,0 +1,6 @@
+#' fun1
+#' @param x numeric
+#' @export
+fun1 <- function(x){
+    rnorm(100*x)
+}
---
+++ R/fun2.R
@@ -0,0 +1,6 @@
+#' fun2
+#' @param y numeric
+#' @export
+fun2 <- function(y){
+    rnorm(200*y)
+}
